7c3e67122c257d966cd2f706be0216b3a775d0d2
Brian Rogers <captbrogers@gmail.com>
2018-10-15 13:36:43 -0600

    Adding shaders and a triangle

--- src/Main.cpp
+++ src/Main.cpp
@@ -14,7 +14,22 @@
 // Window Dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
 
-int main()
+// Shaders
+const GLchar* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 position;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"}\0";
+
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
+int main(int argc, char const *argv[])
 {
     glfwInit();
 
@@ -53,6 +68,81 @@ int main()
     // Define the viewport dimensions
     glViewport ( 0, 0, screenWidth, screenHeight );
 
+    // Build and compile our shader program
+    // Vertext shader
+    GLuint vertexShader = glCreateShader( GL_VERTEX_SHADER );
+    glShaderSource( vertexShader, 1, &vertexShaderSource, NULL );
+
+    glCompileShader( vertexShader );
+    // check for compile time errors
+    GLint success;
+    GLchar infoLog[512];
+
+    glGetShaderiv( vertexShader, GL_COMPILE_STATUS, &success );
+
+    if ( !success )
+    {
+        glGetShaderInfoLog( vertexShader, 512, NULL, infoLog );
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Fragment shader
+    GLuint fragmentShader = glCreateShader( GL_FRAGMENT_SHADER );
+    glShaderSource( fragmentShader, 1, &fragmentShaderSource, NULL );
+    glCompileShader( fragmentShader );
+
+    // Check for compile time errors
+    glGetShaderiv( fragmentShader, GL_COMPILE_STATUS, &success );
+    if ( !success )
+    {
+        glGetShaderInfoLog( fragmentShader, 512, NULL, infoLog );
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Link shaders
+    GLuint shaderProgram = glCreateProgram( );
+    glAttachShader( shaderProgram, vertexShader );
+    glAttachShader( shaderProgram, fragmentShader );
+    glLinkProgram( shaderProgram );
+
+    // Check for linking errors
+    glGetProgramiv( shaderProgram, GL_LINK_STATUS, &success );
+    if ( !success )
+    {
+        glGetProgramInfoLog( shaderProgram, 512, NULL, infoLog );
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+    glDeleteShader( vertexShader );
+    glDeleteShader( fragmentShader );
+
+    GLfloat vertices[] = {
+        -0.5f, -0.5f, -0.0f, // left
+        0.5f, -0.5f, 0.0f,   // right
+        0.0f, 0.5f, 0.0f     // top
+    };
+
+    GLuint VBO, VAO;
+    glGenVertexArrays( 1, &VAO );
+    glGenBuffers( 1, &VBO );
+
+    // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s)
+    glBindVertexArray( VAO );
+
+    glBindBuffer( GL_ARRAY_BUFFER, VBO );
+    glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
+
+    glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof( GLfloat ), ( GLvoid * ) 0 );
+    glEnableVertexAttribArray( 0 );
+
+    glBindBuffer( GL_ARRAY_BUFFER, 0 );
+    // Note that this is allowed, the call to glVertexAttribPointer
+    // registered VBO as the currently bound vertex buffer object so
+    // afterwards we can safely unbind
+
+    glBindVertexArray( 0 );
+    // Unbind VAO (it's always a good ting to unbind any buffer/array to prevent strange bugs)
+
     // Game loop
     while ( !glfwWindowShouldClose( window ) )
     {
@@ -64,9 +154,19 @@ int main()
         glClearColor( 0.2f, 0.3f, 0.3f, 1.0f );
         glClear( GL_COLOR_BUFFER_BIT );
 
+        // Draw our first triangle
+        glUseProgram( shaderProgram );
+        glBindVertexArray( VAO );
+        glDrawArrays( GL_TRIANGLES, 0, 3 );
+        glBindVertexArray( 0 );
+
         // swap the screen buffers
         glfwSwapBuffers( window );
     }
+
+    // Properly de-allocate all resources once they've outlived their purpose
+    glDeleteVertexArrays( 1, &VAO );
+    glDeleteBuffers( 1, &VBO );
 
     // Terminate GLFW, clearing any resources allocated by GLFW.
     glfwTerminate( );
